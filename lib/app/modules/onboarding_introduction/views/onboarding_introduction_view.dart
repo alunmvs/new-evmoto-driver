e3b9174826c51d580a33716a0a1a1356f7494c4c
kenny <kenny@ecgoevmoto.com>
2025-12-01 11:42:51 +0700

    🐛 fix UI onboarding introduction

--- lib/app/modules/onboarding_introduction/views/onboarding_introduction_view.dart
+++ lib/app/modules/onboarding_introduction/views/onboarding_introduction_view.dart
@@ -29,84 +29,87 @@ class OnboardingIntroductionView
                   color: controller.themeColorServices.primaryBlue.value,
                 ),
               )
-            : Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CarouselSlider(
-                      items: controller.systemImageList
-                          .map(
-                            (systemImage) =>
-                                CachedNetworkImage(imageUrl: systemImage.url!),
-                          )
-                          .toList(),
+            : SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CarouselSlider(
+                        items: controller.systemImageList
+                            .map(
+                              (systemImage) => CachedNetworkImage(
+                                imageUrl: systemImage.url!,
+                              ),
+                            )
+                            .toList(),
 
-                      options: CarouselOptions(
-                        onPageChanged: (index, reason) {
-                          controller.indexBanner.value = index.toDouble();
-                        },
-                        height: MediaQuery.of(context).size.height / 2,
-                        enableInfiniteScroll: false,
-                        autoPlay: false,
-                        disableCenter: true,
-                        viewportFraction: 1,
-                        aspectRatio: 720 / 1080,
-                        padEnds: false,
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Center(
-                      child: DotsIndicator(
-                        dotsCount: controller.systemImageList.length,
-                        position: controller.indexBanner.value,
-                        decorator: DotsDecorator(
-                          spacing: EdgeInsets.symmetric(
-                            horizontal: 3,
-                            vertical: 4,
-                          ),
-                          color: controller
-                              .themeColorServices
-                              .neutralsColorGrey300
-                              .value,
-                          activeColor:
-                              controller.themeColorServices.primaryBlue.value,
+                        options: CarouselOptions(
+                          onPageChanged: (index, reason) {
+                            controller.indexBanner.value = index.toDouble();
+                          },
+                          height: MediaQuery.of(context).size.height / 2,
+                          enableInfiniteScroll: false,
+                          autoPlay: false,
+                          disableCenter: true,
+                          viewportFraction: 1,
+                          aspectRatio: 720 / 1080,
+                          padEnds: false,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 12),
-                    SizedBox(
-                      height: 46,
-                      width: MediaQuery.of(context).size.width,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Get.offAndToNamed(Routes.LOGIN);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              controller.themeColorServices.primaryBlue.value,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                            side: BorderSide(
-                              color: controller
-                                  .themeColorServices
-                                  .sematicColorBlue200
-                                  .value,
-                              width: 2,
+                      SizedBox(height: 12),
+                      Center(
+                        child: DotsIndicator(
+                          dotsCount: controller.systemImageList.length,
+                          position: controller.indexBanner.value,
+                          decorator: DotsDecorator(
+                            spacing: EdgeInsets.symmetric(
+                              horizontal: 3,
+                              vertical: 4,
+                            ),
+                            color: controller
+                                .themeColorServices
+                                .neutralsColorGrey300
+                                .value,
+                            activeColor:
+                                controller.themeColorServices.primaryBlue.value,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      SizedBox(
+                        height: 46,
+                        width: MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Get.offAndToNamed(Routes.LOGIN);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                controller.themeColorServices.primaryBlue.value,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              side: BorderSide(
+                                color: controller
+                                    .themeColorServices
+                                    .sematicColorBlue200
+                                    .value,
+                                width: 2,
+                              ),
                             ),
                           ),
-                        ),
-                        child: Text(
-                          "Enter Now",
-                          style: controller
-                              .typographyServices
-                              .bodyLargeBold
-                              .value
-                              .copyWith(color: Colors.white),
+                          child: Text(
+                            "Enter Now",
+                            style: controller
+                                .typographyServices
+                                .bodyLargeBold
+                                .value
+                                .copyWith(color: Colors.white),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
       ),
