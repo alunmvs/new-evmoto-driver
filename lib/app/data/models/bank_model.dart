d1319257ee1e78ac38cfd3696cc30db7b5514045
kenny <kenny@ecgoevmoto.com>
2025-12-10 17:42:48 +0700

    🎨 add API withdraw bank account

---
+++ lib/app/data/models/bank_model.dart
@@ -0,0 +1,18 @@
+class Bank {
+  String? code;
+  String? name;
+
+  Bank({this.code, this.name});
+
+  Bank.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['name'] = this.name;
+    return data;
+  }
+}
