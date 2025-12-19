1ff454a608f426a17a5935aed32e1f6f17eacae6
kenny <kenny@ecgoevmoto.com>
2025-12-19 14:05:42 +0700

    🎨 add UI splash screen

--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:new_evmoto_driver/app/routes/app_pages.dart';
 import 'package:new_evmoto_driver/app/services/theme_color_services.dart';
 import 'package:new_evmoto_driver/app/services/typography_services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreenController extends GetxController {
   final themeColorServices = Get.find<ThemeColorServices>();
@@ -19,15 +18,15 @@ class SplashScreenController extends GetxController {
       var token = await storage.read(key: 'token');
 
       if (token == null || token == "") {
-        var prefs = await SharedPreferences.getInstance();
-        var isOnboardingIntroductionShown = prefs.getBool(
-          'is_onboarding_introduction_shown',
-        );
-        if (isOnboardingIntroductionShown == true) {
-          Get.offAndToNamed(Routes.LOGIN);
-        } else {
-          Get.offAndToNamed(Routes.ONBOARDING_INTRODUCTION);
-        }
+        // var prefs = await SharedPreferences.getInstance();
+        // var isOnboardingIntroductionShown = prefs.getBool(
+        //   'is_onboarding_introduction_shown',
+        // );
+        // if (isOnboardingIntroductionShown == true) {
+        Get.offAndToNamed(Routes.LOGIN);
+        // } else {
+        //   Get.offAndToNamed(Routes.ONBOARDING_INTRODUCTION);
+        // }
       } else {
         Get.offAndToNamed(Routes.HOME);
       }
--- lib/app/modules/splash_screen/views/splash_screen_view.dart
+++ lib/app/modules/splash_screen/views/splash_screen_view.dart
@@ -12,10 +12,30 @@ class SplashScreenView extends GetView<SplashScreenController> {
       () => Scaffold(
         body: controller.isFetch.value
             ? null
-            : const Center(
-                child: Text(
-                  'SplashScreenView is working',
-                  style: TextStyle(fontSize: 20),
+            : Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/img_background_splash_screen.png',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Image.asset(
+                        "assets/logos/logo.png",
+                        width: 100,
+                        height: 100,
+                      ),
+                    ),
+                  ],
                 ),
               ),
       ),
