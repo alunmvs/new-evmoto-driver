d3bad14f157803325b4ff572a2b7e5368027bb86
kenny <kenny@ecgoevmoto.com>
2026-01-02 14:40:15 +0700

    🐛 fix bug localizations

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:get/get.dart';
 import 'package:new_evmoto_driver/app/services/api_services.dart';
@@ -59,6 +60,11 @@ Future<void> main() async {
   runApp(
     GetMaterialApp(
       title: "Evmoto Driver",
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       supportedLocales: const [
         Locale('en', 'US'),
         Locale('id', 'ID'),
